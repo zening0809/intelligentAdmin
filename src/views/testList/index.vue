9b9370b8cd647bd1b5af98f03f5c320efc780091
liuhaonan12 <liuhaonan12@360buyad.local>
2019-12-09 10:50:52 +0800

    提交

---
+++ src/views/testList/index.vue
@@ -0,0 +1,162 @@
+<template>
+  <div>
+    <el-form :inline="true" :model="query" class="demo-form-inline">
+      <el-form-item label="用户">
+        <el-input v-model="query.author" placeholder="请输入用户" />
+      </el-form-item>
+      <el-form-item label="审核人">
+        <el-input v-model="query.reviewer" placeholder="请输入审核人" />
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">查询</el-button>
+        <el-button @click="handleReset">重置</el-button>
+      </el-form-item>
+    </el-form>
+    <!-- 列表 -->
+    <div class="control">
+      <el-table
+        ref="handSelectTest_multipleTable"
+        :data="tableData"
+        style="width: 100%"
+        @current-change="handleRowClick"
+        @selection-change="handleSelectionChange"
+      >
+        <el-table-column>
+          <template slot="header">
+            <div class="btns">
+              <el-button
+                type="primary"
+                @click="()=>{handleFunction1(multipleSelection)}"
+              >{{ $t('testList.function1') }}</el-button>
+              <el-button type="primary">{{ $t('testList.function2') }}</el-button>
+              <el-button type="primary">{{ $t('testList.function3') }}</el-button>
+            </div>
+          </template>
+          <el-table-column type="selection" width="55" />
+          <el-table-column
+            v-for="(item, index) in fields"
+            :key="index"
+            :prop="item.key"
+            :label="item.name"
+            :width="item.width"
+          />
+        </el-table-column>
+      </el-table>
+    </div>
+    <!-- 分页 -->
+    <div class="block">
+      <el-pagination
+        :current-page.sync="currentPage2"
+        :page-sizes="[10, 20]"
+        :page-size="100"
+        layout="sizes, prev, pager, next"
+        :total="total"
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+      />
+    </div>
+  </div>
+</template>
+<script>
+import { testList } from '@/api/testList'
+export default {
+  data() {
+    return {
+      formInline: {
+        user: '',
+        region: ''
+      },
+      currentPage2: 1,
+      total: 0,
+      fields: [
+        { key: 'author', name: '用户' },
+        { key: 'display_time', name: '创建时间' },
+        { key: 'timestamp', name: '运单号' },
+        { key: 'reviewer', name: '审核人' },
+        { key: 'forecast', name: '预收金额' }
+      ],
+      tableData: [],
+      query: {
+        author: undefined,
+        reviewer: undefined
+      },
+      listQuery: {
+        page: 1,
+        limit: 10
+      },
+      multipleSelection: []
+    }
+  },
+  mounted() {
+    this.queryList()
+  },
+  methods: {
+    async queryList(query = {}) {
+      console.log('query', query)
+      await testList({ ...this.listQuery, ...query }).then(
+        ({ code, content }) => {
+          if (code === 1) {
+            this.tableData = content.rows
+            this.total = content.total
+          } else {
+            this.$message.error(content.message)
+          }
+        }
+      )
+    },
+    onSubmit() {
+      console.log(1)
+      this.queryList(this.query)
+    },
+    handleReset() {
+      this.listQuery.page = 1
+      this.currentPage2 = 1
+      this.queryList()
+    },
+    handleSizeChange(val) {
+      //   console.log(`每页 ${val} 条`)
+      this.listQuery.limit = val
+      this.listQuery.page = 1
+      this.currentPage2 = 1
+      this.queryList()
+    },
+    handleCurrentChange(val) {
+      //   console.log(`当前页: ${val}`)
+      this.listQuery.page = val
+      this.queryList()
+    },
+    handleRowClick(row, column, event) {
+      this.$refs.handSelectTest_multipleTable.toggleRowSelection(row)
+      // console.log('this.selecTestContent', this.selectTestContent)
+    },
+    handleSelectionChange(val) {
+      console.log(val)
+      this.multipleSelection = val
+    },
+    handleFunction1(data) {
+      console.log(data, 'data')
+    }
+  }
+}
+</script>
+<style  scoped>
+.demo-form-inline {
+  margin: 20px 40px;
+  display: flex;
+  justify-content: space-around;
+}
+tr {
+  background-color: rgb(245, 247, 250);
+}
+.control {
+  background-color: rgb(245, 247, 250);
+}
+.btns {
+  margin-left: 10px;
+}
+.block {
+  display: flex;
+  text-align: right;
+  justify-content: flex-end;
+}
+</style>
