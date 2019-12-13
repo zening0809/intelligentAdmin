949b54dbdf8f966953c7cc65836cab5df26e78b9
泽宁 <752671125@qq.com>
2019-12-13 15:05:16 +0800

    fix

--- src/router/views/master.js
+++ src/router/views/master.js
@@ -1,16 +1,2 @@
 export default [
-  {
-    path: '/skiude',
-    component: import('@/views/test2'),
-    name: '侧边栏1',
-    title: '侧边栏1',
-    icon: 'tab'
-  },
-  {
-    path: '/adaa',
-    component: import('@/views/testList'),
-    name: '侧边栏2',
-    title: '侧边栏2',
-    icon: 'tab'
-  }
 ]
--- src/router/views_map/master.js
+++ src/router/views_map/master.js
@@ -1,17 +1,3 @@
 export default [
-  {
-    path: '/skiude',
-    component: import('@/views/testList'),
-    name: '侧边栏1',
-    title: 'master1',
-    icon: 'tab'
-  },
-  {
-    path: '/adaa',
-    component: import('@/views/testList'),
-    name: '侧边栏2',
-    title: 'master2',
-    icon: 'tab'
-  },
   '@@@@@'
 ]
